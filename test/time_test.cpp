fd79769dcc14248795a16b3aa1148b5463523648
don.wang <wanguandong@126.com>
2024-04-28 17:29:40 +0800

    perf: test

--- test/time_test.cpp
+++ test/time_test.cpp
@@ -3,14 +3,14 @@
 #include <unistd.h>
 
 int main(int argc, char const *argv[]) {
-  printf("%s\n", Time::Format().c_str());
-  printf("%s\n", Time::Format().c_str());
-  long long startTime = Time::UnixMilli();
-  if (!Time::Since(startTime, 90)) {
+  printf("%s\n", libtime::Format().c_str());
+  printf("%s\n", libtime::Format().c_str());
+  long long startTime = libtime::UnixMilli();
+  if (!libtime::Since(startTime, 90)) {
     printf("%lld\n", startTime);
   }
   sleep(2);
-  if (Time::Since(startTime, 5000)) {
+  if (libtime::Since(startTime, 5000)) {
     printf("5s超时\n");
   } else {
     printf("-------------\n");
