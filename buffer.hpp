33c9bc69975963be917c828a868ca84649cc9ab2
don.wang <wanguandong@126.com>
2024-07-31 09:35:11 +0800

    perf: buffer使用vector

--- buffer.hpp
+++ buffer.hpp
@@ -1,55 +1,37 @@
 #pragma once
 
 #include <assert.h>
-#include <stddef.h>
-#include <stdio.h>
 #include <string.h>
 
 #include <mutex>
 #include <string>
+#include <vector>
 
 namespace libyte {
 class Buffer {
 private:
   std::mutex lock_;
-  char *ptr_;
-  size_t total_;
-  size_t offset_;
+  std::vector<char> v_;
 
 public:
-  Buffer() : offset_(0) {
-    total_ = 2048;
-    ptr_ = new char[total_];
-  }
-  Buffer(size_t n) : offset_(0) {
-    total_ = n;
-    ptr_ = new char[total_];
-  }
-  ~Buffer() {
-    if (ptr_) {
-      delete[] ptr_;
-      ptr_ = NULL;
-    }
-  }
-
+  Buffer() {}
+  Buffer(size_t n) { v_.reserve(n); }
+  ~Buffer() {}
   // 空
-  bool Empty() { return offset_ == 0; }
+  bool Empty() { return v_.empty(); }
   // 容量
-  size_t Cap() { return total_; }
+  size_t Cap() { return v_.capacity(); }
   // 数据长度
-  size_t Len() { return offset_; }
+  size_t Len() { return v_.size(); }
   // 数据
-  char *Bytes() { return ptr_; }
+  char *Bytes() { return v_.data(); }
   // 写入指定长度
   size_t Write(char *p, size_t n) {
     assert(NULL != p || n > 0);
-    return tryGrowWrite(p, n);
+    return tryWrite(p, n);
   }
   // 写入
-  size_t Write(const char *s) {
-    assert(NULL != s);
-    return tryGrowWrite((char *)s, strlen(s));
-  }
+  size_t Write(const char *s) { return Write((char *)s, strlen(s)); }
   // 写入
   template <typename T> size_t Write(T b) {
     return Write((char *)&b, sizeof(T));
@@ -64,57 +46,46 @@ public:
     return tryRead(p, n);
   }
   // 重置
-  size_t Reset(size_t offset = 0) {
-    offset_ = offset;
-    memset(ptr_, 0, total_);
-    return offset_;
-  }
+  void Reset(size_t offset = 0) { v_.resize(offset, 0); }
   // 移除数据
   size_t Remove(size_t n) {
     std::lock_guard<std::mutex> lock(lock_);
-    size_t s = n < offset_ ? n : offset_;
-    offset_ -= s;
-    ::memmove(ptr_, ptr_ + s, offset_);
-    return s;
+    n = n > Len() ? Len() : n;
+    v_.erase(v_.begin(), v_.begin() + n);
+    return n;
   }
   // 修改容量
   void Realloc(size_t n, bool force = false) {
     std::lock_guard<std::mutex> lock(lock_);
-    offset_ = 0;
-    if (force || total_ < n) {
-      total_ = n;
-      ptr_ = (char *)realloc(ptr_, total_);
-      assert(ptr_);
+    if (force || Cap() < n) {
+      v_.reserve(n);
     }
-    ::memset(ptr_, 0, total_);
+    v_.resize(0);
   }
 
 private:
   Buffer(const Buffer &) = delete;
   Buffer &operator=(const Buffer &) = delete;
 
-  size_t tryGrowWrite(char *p, size_t n) {
+  size_t tryWrite(char *p, size_t n) {
     std::lock_guard<std::mutex> lock(lock_);
-    size_t s = offset_ + n;
-    if (s > total_) {
-      total_ += 2 * n;
-      ptr_ = (char *)realloc(ptr_, total_);
-      assert(ptr_);
+    size_t s = Cap();
+    if (Len() + n > s) {
+      s += 2 * n;
+      v_.reserve(s);
     }
-    ::memcpy(ptr_ + offset_, p, n);
-    offset_ += n;
+    v_.insert(v_.end(), p, p + n);
     return n;
   }
 
   size_t tryRead(char *p, size_t n) {
     std::lock_guard<std::mutex> lock(lock_);
-    size_t readSize = n < offset_ ? n : offset_;
-    if (readSize > 0) {
-      ::memcpy(p, ptr_, readSize);
-      offset_ -= readSize;
-      ::memmove(ptr_, ptr_ + readSize, offset_);
+    n = n > Len() ? Len() : n;
+    if (n > 0) {
+      ::memcpy(p, Bytes(), n);
+      v_.erase(v_.begin(), v_.begin() + n);
     }
-    return readSize;
+    return n;
   }
 };
 
